daa38af5b338f46281b536f0664fb972050665f0
AishwaryaMK <aish6mk@gmail.com>
2018-08-18 13:55:12 +0530

    Update Predict_plot.py

--- Predict_plot.py
+++ Predict_plot.py
@@ -1,3 +1,5 @@
+#Step 1:
+#import pakckages
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,14 +7,22 @@ from scipy import stats
 import seaborn as sns
 import statsmodels.api as sm
 from sklearn import linear_model
+#Step 2:
+#Read the dataset
 agri = sns.load_dataset("agriculture_dataset")
 agri.head()
+#Step 3:
+#Plot
 sns.pairplot(agri, hue="Location")
 plt.show()
-sns.pairplot(agri, hue="Crops")
+#Plot
+sns.pairplot(agri, hue="crop")
 plt.show()
+#Step 4
+#Plot 2 variables
 sns.lmplot(x="Production", y="Yield", data=agri)
 plt.show()
+#Step 5
 X = agri[["Production"]]
 y = agri["Yield"]
 model = linear_model.LinearRegression()
